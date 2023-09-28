aa62ff7fd9868a56d3923ee183f29fe99188f8fa
unknown <kagaiquincy@yahoo.com>
2023-09-28 15:39:38 +0300

    first commit

---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,334 @@
+
+
+
+  <aside class="main-sidebar">
+    <!-- sidebar-->
+    <section class="sidebar">	
+		
+        <div class="user-profile">
+			<div class="ulogo">
+				 <a href="index.html">
+				  <!-- logo for regular state and mobile devices -->
+					 <div class="d-flex align-items-center justify-content-center">					 	
+						  <img src="{{ asset('backend/images/logo-dark.png') }}" alt="">
+						  <h3><b>E</b> Market</h3>
+					 </div>
+				</a>
+			</div>
+        </div>
+      
+      <!-- sidebar menu-->
+      <ul class="sidebar-menu" data-widget="tree">  
+		  
+		<li class="#">
+          <a href="{{ url('admin/dashboard') }}">
+            <i data-feather="pie-chart"></i>
+			<span>Dashboard</span>
+          </a>
+        </li>  
+		
+        
+
+
+        <li class="treeview   ">
+          <a href="#">
+            <i data-feather="message-circle"></i>
+            <span>Brands</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class=""><a href="#"><i class="ti-more"></i>All Brand</a></li>
+            
+          </ul>
+        </li> 
+       
+		  
+     
+        <li class="treeview">
+          <a href="#">
+            <i data-feather="mail"></i> <span>Category </span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+         <li class="#"><a href="#"><i class="ti-more"></i>All Category</a></li>
+         <li class="{#"><a href="#"><i class="ti-more"></i>All SubCategory</a></li>
+<li class="#"><a href="#"><i class="ti-more"></i>All Sub->SubCategory</a></li>
+
+
+                      </ul>
+        </li>
+
+        
+
+     
+		
+        <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Products</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="#"><a href="#"><i class="ti-more"></i>Add Products</a></li>
+
+             <li class="#"><a href="#"><i class="ti-more"></i>Manage Products</a></li>
+             
+          </ul>
+        </li> 		  
+
+       
+
+
+
+
+         <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Slider</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class=""><a href=""><i class="ti-more"></i>Manage Slider</a></li>
+
+             
+             
+          </ul>
+        </li>   
+
+         <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Coupons</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class=""><a href=""><i class="ti-more"></i>Manage Coupon</a></li>
+
+             
+             
+          </ul>
+        </li>      
+       
+
+
+         <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Shipping Area</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>Ship Division</a></li>
+
+         <li class=""><a href=""><i class="ti-more"></i>Ship District</a></li>
+
+         <li class=""><a href=""><i class="ti-more"></i>Ship State</a></li>
+
+             
+             
+          </ul>
+        </li>        
+        
+
+
+        <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Manage Blog</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>Blog Category</a></li>
+
+        <li class=""><a href=""><i class="ti-more"></i>List Blog Post</a></li>
+
+         <li class=""><a href=""><i class="ti-more"></i>Add Blog Post</a></li>
+  
+          </ul>
+        </li>       
+
+       
+
+<li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Manage Setting</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href="{"><i class="ti-more"></i>Site Setting</a></li>
+
+        <li class=""><a href=""><i class="ti-more"></i>Seo Setting</a></li>
+ 
+  
+          </ul>
+        </li>
+
+        
+
+        <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Return Order</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>Return Request</a></li>
+
+        <li class=""><a href=""><i class="ti-more"></i>All Request</a></li>
+ 
+  
+          </ul>
+        </li>    
+
+       
+
+
+          <li class="">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Manage Review</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>Pending Review</a></li>
+
+        <li class=""><a href=""><i class="ti-more"></i>Publish Review</a></li>
+ 
+  
+          </ul>
+        </li>    
+
+       
+   
+		 
+        <li class="header nav-small-cap">User Interface</li>
+
+        
+		    
+        <li class="">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Orders </span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>Pending Orders</a></li>
+
+        <li class=""><a href=""><i class="ti-more"></i>Confirmed Orders</a></li>
+
+        <li class=""><a href=""><i class="ti-more"></i>Processing Orders</a></li>
+
+      <li class="{"><a href=""><i class="ti-more"></i> Picked Orders</a></li>
+
+      <li class=""><a href=""><i class="ti-more"></i> Shipped Orders</a></li>
+
+     <li class=""><a href=""><i class="ti-more"></i> Delivered Orders</a></li>
+  
+  <li class=""><a href=""><i class="ti-more"></i> Cancel Orders</a></li>
+
+             
+             
+          </ul>
+        </li>         
+        
+
+
+ <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Manage Stock </span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>Product Stock</a></li>
+
+        
+          </ul>
+        </li>    
+
+		    
+
+		 <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>All Reports </span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>All Reports</a></li>
+
+        
+          </ul>
+        </li>         
+
+     <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>All Users </span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>All Users</a></li>
+
+        
+          </ul>
+        </li>    
+       
+
+
+        <li class="treeview">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Admin User Role </span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class=""><a href=""><i class="ti-more"></i>All Admin User </a></li>
+
+        
+          </ul>
+        </li>    
+
+      </ul>
+    </section>
+	
+	<div class="sidebar-footer">
+		<!-- item-->
+		<a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Settings" aria-describedby="tooltip92529"><i class="ti-settings"></i></a>
+		<!-- item-->
+		<a href="mailbox_inbox.html" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i class="ti-email"></i></a>
+		<!-- item-->
+		<a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i class="ti-lock"></i></a>
+	</div>
+  </aside>
---
+++ resources/views/frontend/profile/user_profile.blade.php
@@ -0,0 +1,85 @@
+@extends('frontend.main_master')
+@section('content')
+
+<div class="body-content">
+	<div class="container">
+		<div class="row">
+      
+			 @include('frontend.common.user_sidebar')
+
+			<div class="col-md-2">
+				
+			</div> <!-- // end col md 2 -->
+
+
+			<div class="col-md-6">
+  <div class="card">
+  	<h3 class="text-center"><span class="text-danger">Hi....</span><strong>{{ Auth::user()->name }}</strong> Update Your Profile  </h3>
+
+  	<div class="card-body">
+  		
+
+
+  		<form method="post" action="{{ route('user.profile.store') }}" enctype="multipart/form-data">
+  			@csrf
+
+
+         <div class="form-group">
+            <label class="info-title" for="exampleInputEmail1">Name <span> </span></label>
+            <input type="text" name="name" class="form-control" value="{{ $user->name }}">
+        </div>
+
+        <div class="form-group">
+            <label class="info-title" for="exampleInputEmail1">Email <span> </span></label>
+            <input type="email" name="email" class="form-control" value="{{ $user->email }}">
+        </div>
+
+
+        <div class="form-group">
+            <label class="info-title" for="exampleInputEmail1">Phone <span> </span></label>
+            <input type="text" name="phone" class="form-control" value="{{ $user->phone }}">
+        </div>
+
+        <div class="form-group">
+            <label class="info-title" for="exampleInputEmail1">User Image <span> </span></label>
+            <input type="file" name="profile_photo_path" class="form-control">
+        </div>
+        <div class="form-group">
+            <img id="showImage" src="{{ (!empty($user->profile_photo_path))? url('upload/user_images/'.$user->profile_photo_path):url('upload/no_image.jpg') }}" style="width: 100px; height: 100px;"> 
+        </div>
+
+         <div class="form-group">            
+           <button type="submit" class="btn btn-danger">Update</button>
+        </div>         
+ 
+
+  			
+  		</form>  		
+  	</div>
+
+ 
+  	
+  </div>
+				
+			</div> <!-- // end col md 6 -->
+			
+		</div> <!-- // end row -->
+		
+	</div>
+	
+</div>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#image').change(function(e){
+      var reader = new FileReader();
+      reader.onload = function(e){
+       $('#showImage').attr('src',e.target.result); 
+      }
+      reader.readAsDataURL(e.target.files['0']);
+    });
+  });
+</script>
+ 
+
+@endsection
